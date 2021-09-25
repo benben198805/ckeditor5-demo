60ac0ac313177b44fba475d1044debb9bb956857
qlyan <qlyan@thoughtworks.com>
2021-09-25 19:46:43 +0800

    add ckeditor5 and plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import * as ClassicEditor from '@nvdunginest/ckeditor5-custom-build'
+
+import jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ckeditor-demo';
+  public Editor = ClassicEditor as any;
+  content = ""
+  @ViewChild('myEditor') myEditor: any;
+
+
+  private getArticleContent() {
+    if (this.myEditor && this.myEditor.editorInstance) {
+      return this.myEditor.editorInstance.getData();
+    }
+
+    return '';
+  }
+
+  getContent() {
+    this.content = this.getArticleContent();
+  }
+
+  htmlToPDF() {
+    // parentdiv is the html element which has to be converted to PDF
+    html2canvas(document.querySelector("#result")).then(canvas => {
+
+      var pdf = new jspdf('p', 'pt', [canvas.width, canvas.height]);
+
+      var imgData = canvas.toDataURL("image/jpeg", 1.0);
+      pdf.addImage(imgData, 0, 0, canvas.width, canvas.height);
+      pdf.save('converteddoc.pdf');
+    });
+
+  }
 }
